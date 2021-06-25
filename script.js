4f501e150fcac81227a26513998446b0d625a5f8
Bruno Bandeira <brunodev29ab@gmail.com>
2021-06-24 23:45:00 -0300

    Calculo do orçamento em JS -  Parte 01

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ hamburguer.addEventListener("click", function(){
 });
 
 //AddEventListener é utilizado para adicionar eventos em elementos HTML. Em tempo de execução, você pode utilizar esta função para dizer o que vai acontecer quando o usuário clicar em um botão
+
+
+const qtde = document.querySelector("#qtde").addEventListener("change", atualizarPreco) //Sempre que acontecer um click em #qtde, vai atualizar a caixa de texto
+
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+
+function atualizarPreco() {
+
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    let preco = qtde * 100;
+    if(temJS) preco  {
+        preco = preco + (preco * 10/100)
+    }
+    document.querySelector("#preco").innerHTML ="R$ " + preco.toFixed(2)
+}
+//document.querySelector("#qtde"): Retorna o elemento HTML, ou no nosso caso nossa caixa de texto.
+
+//value:Diferente do comando de cima, esse comando, pega o valor digitado na caixa de texto
