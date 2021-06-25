2680c66e8bd63ee2fddc5da3648e231221091f45
Bruno Bandeira <brunodev29ab@gmail.com>
2021-06-25 12:25:24 -0300

    Portifólio finalizado

--- script.js
+++ script.js
@@ -11,15 +11,31 @@ hamburguer.addEventListener("click", function(){
 const qtde = document.querySelector("#qtde").addEventListener("change", atualizarPreco) //Sempre que acontecer um click em #qtde, vai atualizar a caixa de texto
 
 document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function() {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+} )
+
+
+
 
 function atualizarPreco() {
 
     const qtde = document.querySelector("#qtde").value
     const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
     let preco = qtde * 100;
-    if(temJS) preco  {
-        preco = preco + (preco * 10/100)
-    }
+    if(temJS) preco *= 1.1;
+    if (incluiLayout) preco += 500;
+    let taxaDeUrgencia = 1 - prazo*0.1;
+    preco *= 1 +  taxaDeUrgencia
+    
+    
     document.querySelector("#preco").innerHTML ="R$ " + preco.toFixed(2)
 }
 //document.querySelector("#qtde"): Retorna o elemento HTML, ou no nosso caso nossa caixa de texto.
